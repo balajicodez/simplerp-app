8f76b452515a6785936fa22e2ccd772c789e1f54
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2026-01-02 12:17:25 +0530

    feat(login):token support

--- src/_components/protected/ProtectedRoute.jsx
+++ src/_components/protected/ProtectedRoute.jsx
@@ -2,8 +2,8 @@ import {Navigate} from "react-router-dom";
 import {useAuth} from "../../hooks/useAuth";
 
 export const ProtectedRoute = ({ children }) => {
-    const { session } = useAuth();
-    if (!session && false) {
+    const token = localStorage.getItem('token');
+    if (!token) {
         // user is not authenticated
         return <Navigate to="/login" />;
     }
