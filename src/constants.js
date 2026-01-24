b933939f28eb6107cc77e5ca8f96981bcf97f0be
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2026-01-24 16:22:31 +0530

    feat: login page changes

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,14 @@
 // Use a relative prefix in development so the CRA dev server proxy can forward requests
 // to the API and avoid CORS. In production this should be the real API origin.
-//export const APP_SERVER_URL_PREFIX = 'http://simplerp.sridivyasarees.com:9090/simplerp/api';
+export let APP_SERVER_URL_PREFIX = 'http://simplerp.sridivyasarees.com:9090/simplerp/api';
 export const DAY_CLOSING_WHATSAPP_NUMBERS_CSV = '9740665561,9866472624,9948011234,8985221844';
- 
-export const APP_SERVER_URL_PREFIX = 'http://localhost:9090/simplerp/api';
+
+
+if (process.env.REACT_APP_API_URL) {
+    // create .env with REACT_APP_API_URL='http://localhost:9090/simplerp/api'
+    // for local development
+    APP_SERVER_URL_PREFIX = process.env.REACT_APP_API_URL;
+}
 //export const DAY_CLOSING_WHATSAPP_NUMBERS_CSV = '9740665561';
 
 
